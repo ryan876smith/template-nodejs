3de9faffea42d847a8bf884676142c057349001d
Ryan Smith <itsjstme@gmail.com>
2023-10-16 17:52:30 -0400

    Update app.js
    Signed-off-by: Ryan Smith <itsjstme@gmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
+const http = require('http');
 const express = require('express');
+const socketIo = require('socket.io');
+
 const app = express();
-const port = process.env.PORT ?? 3000;
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.use(express.static('public'))
+const connectedUsers = {};
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Handle new user connections
+  socket.on('new user', (username) => {
+    connectedUsers[socket.id] = username;
+    io.emit('user list', Object.values(connectedUsers));
+  });
+
+  // Handle chat messages
+  socket.on('chat message', (message) => {
+    const sender = connectedUsers[socket.id];
+    io.emit('chat message', { sender, message });
+  });
+
+  socket.on('disconnect', () => {
+    const username = connectedUsers[socket.id];
+    delete connectedUsers[socket.id];
+    io.emit('user list', Object.values(connectedUsers));
+    console.log(`${username} disconnected`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
